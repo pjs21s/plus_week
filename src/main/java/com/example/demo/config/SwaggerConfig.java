fe705715930757eb8bca3a32e86e3e6690889c94
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2024-12-12 21:12:37 +0900

    fetch: swagger 적용

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//http://localhost:8080/swagger-ui/index.html#/
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("데모 프로젝트 API Document")
+                .version("v0.0.1")
+                .description("데모 프로젝트의 API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
